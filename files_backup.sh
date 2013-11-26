7431d1eaeea34d83b7275b6f19da1a731160a1e5
the coder <msofyancs@gmail.com>
2013-11-26 23:08:52 +0700

    rename backup_files

---
+++ files_backup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# This Script used to Backup Files
+# You can Change backup_files and destination to backup your files
+# Please keep Simple and Clean Code
+# If You have Question Please contact me (msofyancs@gmail.com)
+
+# What to backup. 
+backup_files="/apache2/htdocs/"
+
+# Create archive filename.
+day=$(date +%A)
+month=$(date +%m)
+year=$(date +%Y)
+flag="$day-$month-$year"
+archive_file="files.tgz"
+
+# Where to backup 
+dest="/home/backup/www"
+
+# Create Folder by Date
+mkdir -p "$backup_files/$flag"
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$flag/$archive_file"
+date
+echo
+
+# Backup the files using tar.
+tar czf $dest/$flag/$archive_file $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+date
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
